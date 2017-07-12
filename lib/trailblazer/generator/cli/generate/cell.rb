5dd075fab82bc2579d628084e8b6edf95ff9806d
emaglio <emanuele.magliozzi@gmail.com>
2017-07-12 14:57:01 +1000

    Template engine option implemented

--- lib/trailblazer/generator/cli/generate/cell.rb
+++ lib/trailblazer/generator/cli/generate/cell.rb
@@ -4,10 +4,26 @@ module Trailblazer
       desc "cell NAME", "Generates cell file"
       long_desc <<-CELL_LONG_DESC
 
-      `generate cell` generate cell file
+      `generate cell` generate cell and view file
+
+      Actions Available:
+        * new
+        * show
+        * index (will generate item as well)
+        * edit
+        * generic (if name doesn't match above, is generic)
+
+      Template Engine for view:
+        * default => erb
+
+      Examples:
+        `generate cell BlogPost --actions index,foo --template_engine slim`
+
+        Will generate an index, item and foo cell files and the corrisponding template using slim
 
       CELL_LONG_DESC
       options actions: :required
+      option :template_engine, default: "erb"
       def cell(name)
         Trailblazer::Generator::Builder::Cell.(name: name, options: options)
       end
