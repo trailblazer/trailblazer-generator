e02d4fa92a67cf078e416613fa5ff671a86a11d0
emaglio <emanuele.magliozzi@gmail.com>
2017-07-15 13:46:54 +1000

    No output file during test for Cell and View

--- lib/trailblazer/generator/builder/cell.rb
+++ lib/trailblazer/generator/builder/cell.rb
@@ -4,14 +4,28 @@ class Trailblazer::Generator::Builder::Cell < Trailblazer::Operation
 
   step Trailblazer::Generator::Macro::ValidateClassName()
   step :generate_actions!
+  step :create_files!
   step :generate_views!
   failure Trailblazer::Generator::Macro::Failure()
 
   def generate_actions!(options, params:)
     actions = params[:options]['actions'].split(',')
+    params["cell_result"] = {"files_content" => [], "files_path" => []}
     actions.each do |action|
-      generate_file(options, name: params[:name], action: action)
-      generate_file(options, name: params[:name], action: 'item') if action == 'Index'
+      params = generate_file(options, params: params, action: action)
+      params = generate_file(options, params: params, action: 'item') if action == 'Index'
+    end
+  end
+
+  def create_files!(options, params:)
+    debug = (params[:options]["debug"] ||= false)
+    return true if debug
+
+    contents = params["cell_result"]["files_content"]
+    paths = params["cell_result"]["files_path"]
+
+    contents.zip(paths).each do |content, path|
+      Trailblazer::Generator::Output.new(path: path, content: content).save
     end
   end
 
@@ -22,21 +36,24 @@ class Trailblazer::Generator::Builder::Cell < Trailblazer::Operation
       actions << ',item'
       options_dup['actions'] = actions
     end
-    Trailblazer::Generator::Builder::View.(name: params[:name], options: options_dup)
+    result = Trailblazer::Generator::Builder::View.(name: params[:name], options: options_dup)
+    params["view_result"] = result["params"]["view_result"]
     true
   end
 
   private
-    def generate_file(options, name:, action:)
+    def generate_file(options, params:, action:)
       model = Trailblazer::Generator::Cell.build_model(
-        name: name, action: action
+        name: params[:name], action: action
       )
-      params = options['params'][:options]
-      content = Cell.(model, params)
+      content = Cell.(model, params[:options])
 
-      name = Trailblazer::Generator::Inflector.underscore(name)
-      path = File.join('app', 'concepts', name, 'cell', "#{action}.rb")
+      params["cell_result"]["files_content"] << content.show
 
-      Trailblazer::Generator::Output.new(path: path, content: content).save
+      name = Trailblazer::Generator::Inflector.underscore(params[:name])
+      path = File.join('app', 'concepts', name, 'cell', "#{action.downcase}.rb")
+
+      params["cell_result"]["files_path"] << path
+      return params
     end
 end
--- lib/trailblazer/generator/builder/view.rb
+++ lib/trailblazer/generator/builder/view.rb
@@ -4,26 +4,43 @@ class Trailblazer::Generator::Builder::View < Trailblazer::Operation
 
   step Trailblazer::Generator::Macro::ValidateClassName()
   step :generate_actions!
+  step :create_files!
   failure Trailblazer::Generator::Macro::Failure()
 
   def generate_actions!(options, params:)
     actions = params[:options]['actions'].split(',')
+    params["view_result"] = {"files_content" => [], "files_path" => []}
     actions.each do |action|
-      generate_file(options, name: params[:name], action: action)
+      generate_file(options, params: params, action: action)
+    end
+  end
+
+  def create_files!(options, params:)
+    debug = (params[:options]["debug"] ||= false)
+    return true if debug
+
+    contents = params["view_result"]["files_content"]
+    paths = params["view_result"]["files_path"]
+
+    contents.zip(paths).each do |content, path|
+      Trailblazer::Generator::Output.new(path: path, content: content).save
     end
   end
 
   private
-    def generate_file(options, name:, action:)
+    def generate_file(options, params:, action:)
       model = Trailblazer::Generator::Cell.build_model(
-        name: name, action: action
+        name: params[:name], action: action
       )
-      params = options['params'][:options]
-      content = Cell.(model, params)
+      content = Cell.(model, params[:options])
 
-      name = Trailblazer::Generator::Inflector.underscore(name)
-      path = File.join('app', 'concepts', name, 'view', "#{action}.erb")
+      params["view_result"]["files_content"] << content.show
 
-      Trailblazer::Generator::Output.new(path: path, content: content).save
+      name = Trailblazer::Generator::Inflector.underscore(params[:name])
+      path = File.join('app', 'concepts', name, 'view', "#{action.downcase}.erb")
+
+      params["view_result"]["files_path"] << path
+
+      return params
     end
 end
