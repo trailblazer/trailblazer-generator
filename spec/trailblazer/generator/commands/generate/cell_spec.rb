14192f3e4d09a093b864679dfff8a0541098a24a
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2018-05-18 11:59:27 +0800

    Set --view=slim as default - passing --view=none will not create the view file

--- spec/trailblazer/generator/commands/generate/cell_spec.rb
+++ spec/trailblazer/generator/commands/generate/cell_spec.rb
@@ -5,17 +5,18 @@ require "./spec/trailblazer/generator/commands/shared_example_for_command"
 RSpec.describe Trailblazer::Generator::Commands::Generate::Cell do
   let(:command) { described_class }
 
-  it_behaves_like "a single file generation command", "cell"
+  it_behaves_like "a single file generation command", "cell", true
 
-  context "when passing --view option creates the view file" do
-    let(:run_command) { `bin/trailblazer g cell SharedExample New --view=slim` }
-    let(:file) { Pathname.new("./app/concepts/shared_example/view/new.slim") }
+  context "when passing none to --view option" do
+    let(:run_command) { `bin/trailblazer g cell SharedExample SomeStuff --view=none` }
+    let(:cell_file) { Pathname.new("./app/concepts/shared_example/cell/some_stuff.rb") }
+    let(:view_file) { Pathname.new("./app/concepts/shared_example/view/some_stuff.slim") }
 
-    it "and shows create messages" do
-      expect(run_command).to include "Starting Generator for Trailblazer View"
-      expect(run_command).to include "Create"
-      expect(run_command).to include "app/concepts/shared_example/view/new.slim"
-      expect(Pathname(file).exist?).to eq true
+    it "does not create the view file" do
+      run_command
+
+      expect(Pathname(cell_file).exist?).to eq true
+      expect(Pathname(view_file).exist?).to eq false
     end
   end
 end
